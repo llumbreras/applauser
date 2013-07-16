8f0c1b4054af99a2e78ba7a9978a87ff51023215
L Lumbreras <llumbreras@gmail.com>
2013-07-16 12:52:05 -0700

    Add monban scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Applauser::Application.routes.draw do
+  resource :session, only: [:new, :create, :destroy]
+
+  resources :users, only: [:new, :create]
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
