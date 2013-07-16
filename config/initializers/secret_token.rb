bbc5a01d9010396dd3cd09ce4c8f40d6bfb3e833
L Lumbreras <llumbreras@gmail.com>
2013-07-16 10:32:20 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Applauser::Application.config.secret_token = 'fa379d0a7d35c769ab88bce5734009b879662f043d0bc7e0b91ccb5353b47718b63da0fab506a8de1c78ec6ca7002af2e1d86fe84a5fee0e73bc626b0e8d131a'
