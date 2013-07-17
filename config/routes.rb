aba85111cb02a0739172e2ad5ffd1b01920c41ed
L Lumbreras <llumbreras@gmail.com>
2013-07-16 18:33:52 -0700

    Add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Applauser::Application.routes.draw do
   root to: 'homes#show', via: :get
+  resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 
