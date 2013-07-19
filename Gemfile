9ca5a807784b861ce371e70b587f46b8bc0a7d9f
L Lumbreras <llumbreras@gmail.com>
2013-07-18 21:09:04 -0700

    Add initializer

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'sqlite3'
 gem 'strong_parameters', '0.2.0'
 gem 'monban', '0.0.6'
 gem 'paperclip', '3.4.0'
-
+gem 'rmagick', '2.13.2'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- app/models/photo_applause.rb
+++ app/models/photo_applause.rb
@@ -1,6 +1,7 @@
 class PhotoApplause < ActiveRecord::Base
 
-	has_attached_file :image, styles: {
-		applause: "200x200>"
-	}
+	has_attached_file :image,
+	#url: "/system/photo_applauses/images/000/000/:id/original/:basename.:extension",
+	#path: "system/photo_applauses/images/000/000/:id/original/:basename.:extension",
+	styles: {	applause: "200x200>"	}
 end
