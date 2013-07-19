86ba0554ea291e13e05b9cbfba9804d49430118b
L Lumbreras <llumbreras@gmail.com>
2013-07-18 21:47:09 -0700

    Add helper for follow button

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Applauser::Application.routes.draw do
   root to: 'homes#show', via: :get
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show] do
+  	post 'follow' => 'following_relationships#create'
+  end
   resources :applauses, only: [:show]
   resources :text_applauses, only: [:create]
   resources :photo_applauses, only: [:create]
