1827764815e3295b69bc6424cb33345c9d5f329a
L Lumbreras <llumbreras@gmail.com>
2013-07-22 19:12:08 -0700

    Update routes to include index for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Applauser::Application.routes.draw do
   root to: 'homes#show', via: :get
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show] do
+  resources :users, only: [:index, :new, :create, :show] do
   	post 'follow' => 'following_relationships#create'
   end
   resources :applauses, only: [:show]
