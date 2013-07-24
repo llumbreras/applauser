a2ccd94140876d657ba6b77779a39152ffb2f662
L Lumbreras <llumbreras@gmail.com>
2013-07-23 19:49:00 -0700

    Add notice of follow/unfollow

--- app/controllers/following_relationships_controller.rb
+++ app/controllers/following_relationships_controller.rb
@@ -1,12 +1,12 @@
 class FollowingRelationshipsController < ApplicationController
 	def create
 		current_user.follow user
-		redirect_to user
+		redirect_to user, notice: "Successfully followed!"
 	end
 
 	def destroy
 		current_user.unfollow user
-		redirect_to user
+		redirect_to user, notice: "Successfully unfollowed!"
 	end
 
 	private
