eae1c3ac8dc191844dd14b9ffaccab980c6642a7
L Lumbreras <llumbreras@gmail.com>
2013-07-24 18:57:07 -0700

    Include followed user applause on timeline

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -6,7 +6,13 @@ class Timeline
 	end
 
 	def applauses
-		@user.applauses
+		Applause.where(user_id: applause_user_ids)
+	end
+
+	private
+
+	def applause_user_ids
+		[@user.id] + @user.followed_user_ids
 	end
 
 end
