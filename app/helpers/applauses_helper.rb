f4b1fce3e7f916d8f835273a588264d34b997839
L Lumbreras <llumbreras@gmail.com>
2013-07-29 20:19:30 -0700

    Include hash tag option to search

--- app/helpers/applauses_helper.rb
+++ app/helpers/applauses_helper.rb
@@ -5,4 +5,15 @@ module ApplausesHelper
 		digest = Digest::MD5.hexdigest(user.email)
 		image_tag("http://gravatar.com/avatar/#{digest}?s=#{size}")
 	end
+
+	def applauserize text
+		link_hashtags(text)
+	end
+
+	private
+
+	def link_hashtags text
+		text.gsub(/#\w+/) { |match| link_to(match, hashtag_path(match))}
+	end
+
 end
